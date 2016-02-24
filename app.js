82b1b3fe2ca66e35d0e0ac207e118fbd5d4dc390
Tuesday-Last <sgnortz@gmail>
2016-02-23 20:43:48 -0800

    Text box disabled until Timer starts and is set as the focus on start

--- app.js
+++ app.js
@@ -52,10 +52,11 @@ var Game = {
   elButton: document.getElementById("buttonHolder"),
   elBody: document.getElementById("mainBody"),
 
-  gameStarter: function() {
+    gameStarter: function() {
     Buttons.begin();
     Game.elTextBlock.innerHTML = "Get to ready to start! Click the button to start.";
     Game.elMinion.innerHTML = "Minion Hole";
+    Game.elSubmit.disabled = true;
   },
 
   gamePlayer: function(e) {
@@ -67,10 +68,9 @@ var Game = {
       console.log("gamePlayer triggered.")
       Buttons.kill();
       Game.elSubmit.addEventListener("keypress", Game.captureText, true);
-      console.log("start clock");
       Game.renderMinion();
       startClock();
-      // delay display
+      console.log("start clock");
     };
   },
 
@@ -136,6 +136,7 @@ var Game = {
       console.log("roundNumber: " + Game.roundNumber);
       Game.roundRunning = false;
       Buttons.nextround();
+      // Game.elSubmit.disabled = true;
     } else if (Game.results >= 10) {
       ame.elTextBlock.innerHTML = "You Lose! To many errors, please type the text exactly as you see it here."
       console.log("Round lost")
@@ -154,12 +155,13 @@ function lose(){
       console.log("roundNumber: " + Game.roundNumber);
       resultsArray = [];
       console.log(resultsArray);
+      Game.elSubmit.disabled = true;
 }
 
 function startClock(){
   
   to1 = function(){
-    setTimeout("timerEl.innerHTML = 'Get Ready'", 1000);
+    setTimeout("timerEl.innerHTML = 'Get Ready'", 250);
   };
   to2 = function(){
     setTimeout("timerEl.innerHTML = 'Get Set!'", 2000);
@@ -199,13 +201,15 @@ function myTimer(duration, display) {
 }
 
 function startTimer() {
-    var characterCount = Game.textA.length / 1.5;
-    var difficulty = (Game.roundNumber * 1.25 + 10) * .01;
-    var setDuration = characterCount - Math.floor(difficulty * characterCount);
-        display = timerEl;
-    console.log("difficulty " + difficulty);
-    console.log("Time " + setDuration);
-    myTimer(setDuration, display);
+  Game.elSubmit.disabled = false;
+  Game.elSubmit.focus();
+  var characterCount = Game.textA.length / 1.5;
+  var difficulty = (Game.roundNumber * 1.25 + 10) * .01;
+  var setDuration = characterCount - Math.floor(difficulty * characterCount);
+      display = timerEl;
+  console.log("difficulty " + difficulty);
+  console.log("Time " + setDuration);
+  myTimer(setDuration, display);
 };
 
 var Buttons = {
